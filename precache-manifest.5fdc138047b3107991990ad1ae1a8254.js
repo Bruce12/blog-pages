cc7bab4fc88ef34e9efb98db0538d36de0cc7724
lanjianhua <lanjianhua@ymt360.com>
2020-07-12 23:42:58 +0800

    test

---
+++ precache-manifest.5fdc138047b3107991990ad1ae1a8254.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "69c11a360deaf2bf02c1",
+    "url": "css/vender.5215f07f.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "69c11a360deaf2bf02c1",
+    "url": "js/vender.js?v=d95957"
+  },
+  {
+    "revision": "d3a7e2770fb3a58092bd",
+    "url": "css/app.aa620b9e.css"
+  },
+  {
+    "revision": "02f26813a0305fa62f35",
+    "url": "css/chunk-elementUI.a3ca887d.css"
+  },
+  {
+    "revision": "02f26813a0305fa62f35",
+    "url": "js/chunk-elementUI.js?v=d95957"
+  },
+  {
+    "revision": "a33153ec2d75574497b8",
+    "url": "css/chunk-libs.89bcc4e9.css"
+  },
+  {
+    "revision": "a33153ec2d75574497b8",
+    "url": "js/chunk-libs.js?v=d95957"
+  },
+  {
+    "revision": "4e634a8b667c7e9d66ac",
+    "url": "js/runtime.js?v=d95957"
+  },
+  {
+    "revision": "b687494bbdbe2b9aefda",
+    "url": "js/admin.js?v=d95957"
+  },
+  {
+    "revision": "d3a7e2770fb3a58092bd",
+    "url": "js/app.js?v=d95957"
+  },
+  {
+    "revision": "f27fe2b3c7bb78442214",
+    "url": "css/vendors~vender.543da896.css"
+  },
+  {
+    "revision": "f27fe2b3c7bb78442214",
+    "url": "js/vendors~vender.js?v=d95957"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "b62e1516e8bb12625e59066832ce6f6f",
+    "url": "index.html"
+  },
+  {
+    "revision": "b687494bbdbe2b9aefda",
+    "url": "css/admin.d459db29.css"
+  },
+  {
+    "revision": "328663df3ecbc5a15d71534192000c49",
+    "url": "admin.html"
+  }
+];
